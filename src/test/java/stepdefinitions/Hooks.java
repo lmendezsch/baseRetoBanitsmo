0d26c0e1cc4a39066ca730533edbeee484738f68
lmendezs <lmendezs@choucairtesting.com>
2024-06-14 12:28:37 -0500

    Configuración de base para construcción de un proyecto con sus respectivas dependencias

---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,19 @@
+package stepdefinitions;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+import static net.serenitybdd.core.Serenity.getDriver;
+
+public class Hooks {
+    @Before
+    public static void setup() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @After
+    public void closeBrowser() {
+        getDriver().quit();
+    }
+}
